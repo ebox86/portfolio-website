1a15e21c2ae951542e19494a82f409ae8ca82936
ebox86 <ebox86@gmail.com>
2026-01-01 07:36:58 -0500

    added social links as new routes under /social

--- next.config.js
+++ next.config.js
@@ -8,6 +8,30 @@ const nextConfig = {
 		domains: ['cdn.sanity.io','cdn2.thecatapi.com','http.cat','localhost','media.giphy.com'],
 	},
 	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+	async redirects() {
+		return [
+			{
+				source: '/social/linkedin',
+				destination: 'https://www.linkedin.com/in/evan-kohout/',
+				permanent: true,
+			},
+			{
+				source: '/social/github',
+				destination: 'https://github.com/ebox86',
+				permanent: true,
+			},
+			{
+				source: '/social/x',
+				destination: 'https://twitter.com/ebox86',
+				permanent: true,
+			},
+			{
+				source: '/social/twitter',
+				destination: 'https://twitter.com/ebox86',
+				permanent: true,
+			},
+		];
+	},
 }
 
 module.exports = withMDX(nextConfig);
